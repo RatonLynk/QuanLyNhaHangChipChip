1020a022b9abeb0282e8d00ddaefb713ae56fc6f
khonglotim123 <xagaph13499@fpt.edu.vn>
2021-11-09 22:58:33 +0700

    xà test đẩy code

--- 3_GUI/FrmHome.cs
+++ 3_GUI/FrmHome.cs
@@ -27,7 +27,7 @@ namespace _3_GUI
         private void timer_Tick(object sender, EventArgs e)
         {   
             labGio.Text = DateTime.Now.ToString("HH:mm");
-            //test
+            //test đ
         }
 
 
