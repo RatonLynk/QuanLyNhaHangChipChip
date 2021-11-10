50932173b151063fa9a5f83f26e12b0532d5b58a
Vu:D <ratonlynkz@gmail.com>
2021-11-10 08:49:19 +0700

    Revert "IBUSService"
    This reverts commit aed9be8b6530bf12bbfbfd8445e7c3bc5818c599.

--- 2_BUS/BUSServices/Class1.cs
+++ 2_BUS/BUSServices/Class1.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _1_DAL.iDALServices;
-using _1_DAL.Models;
-using _1_DAL.DALServices;
-using _2_BUS.iBUSServices;
 
 namespace _2_BUS.BUSServices
 {
-    public class QLMenuService : iQLMenuService
+    class Class1
     {
-        
-
     }
 }
