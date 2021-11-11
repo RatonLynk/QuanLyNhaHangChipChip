e799fa7745c5554292f55310bdd38cd68b064b78
khonglotim123 <xagaph13499@fpt.edu.vn>
2021-11-11 09:34:20 +0700

    ql hóa đơn

---
+++ 1_DAL/iDALServices/IHoaDonChiTietService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1_DAL.Models;
+
+namespace _1_DAL.iDALServices
+{
+   public interface IHoaDonChiTietService
+    {
+        public string AddHoaDon(HoaDonChiTiet HoaDon);
+        public string UpdateHoaDon(HoaDonChiTiet HoaDon);
+        public string DeleteHoaDon(HoaDonChiTiet HoaDon);
+        public string SaveHoaDon();
+        public List<HoaDonChiTiet> GetHoaDonCTFromDB();
+    }
+}
