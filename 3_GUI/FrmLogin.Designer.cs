2c3bee58166ca0e4f4b9c986c5ca81daec11e24c
Phong <lovanphong2299@gmail.com>
2021-11-12 15:06:31 +0700

    làm xong GUI login

---
+++ 3_GUI/FrmLogin.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace _3_GUI
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkQMK = new System.Windows.Forms.LinkLabel();
+            this.lblCapsLock = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Footlight MT Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtUsername.Location = new System.Drawing.Point(106, 116);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(277, 37);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Enter += new System.EventHandler(this.txtUsername_Enter);
+            this.txtUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyDown);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUsername_Leave);
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassWord.Font = new System.Drawing.Font("Footlight MT Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPassWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtPassWord.Location = new System.Drawing.Point(106, 176);
+            this.txtPassWord.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassWord.Multiline = true;
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(277, 37);
+            this.txtPassWord.TabIndex = 3;
+            this.txtPassWord.Enter += new System.EventHandler(this.txtPassWord_Enter);
+            this.txtPassWord.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassWord_KeyDown);
+            this.txtPassWord.Leave += new System.EventHandler(this.txtPassWord_Leave);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.Color.White;
+            this.btnDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDangNhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangNhap.Font = new System.Drawing.Font("Footlight MT Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDangNhap.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDangNhap.Image = ((System.Drawing.Image)(resources.GetObject("btnDangNhap.Image")));
+            this.btnDangNhap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangNhap.Location = new System.Drawing.Point(66, 293);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(127, 54);
+            this.btnDangNhap.TabIndex = 4;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 116);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(55, 176);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.White;
+            this.btnThoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnThoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThoat.Font = new System.Drawing.Font("Footlight MT Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnThoat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(256, 293);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(127, 54);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Footlight MT Light", 29.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(147, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 52);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Login";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.linkQMK);
+            this.panel2.Controls.Add(this.txtPassWord);
+            this.panel2.Controls.Add(this.btnThoat);
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Controls.Add(this.btnDangNhap);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(13, 14);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(421, 442);
+            this.panel2.TabIndex = 10;
+            // 
+            // linkQMK
+            // 
+            this.linkQMK.AutoSize = true;
+            this.linkQMK.Location = new System.Drawing.Point(263, 232);
+            this.linkQMK.Name = "linkQMK";
+            this.linkQMK.Size = new System.Drawing.Size(120, 20);
+            this.linkQMK.TabIndex = 9;
+            this.linkQMK.TabStop = true;
+            this.linkQMK.Text = "Quên mật khẩu ?";
+            // 
+            // lblCapsLock
+            // 
+            this.lblCapsLock.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCapsLock.ForeColor = System.Drawing.Color.White;
+            this.lblCapsLock.Location = new System.Drawing.Point(4, 460);
+            this.lblCapsLock.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCapsLock.Name = "lblCapsLock";
+            this.lblCapsLock.Size = new System.Drawing.Size(443, 31);
+            this.lblCapsLock.TabIndex = 11;
+            this.lblCapsLock.Text = "capslock";
+            this.lblCapsLock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmLogin
+            // 
+            this.AcceptButton = this.btnDangNhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.CancelButton = this.btnThoat;
+            this.ClientSize = new System.Drawing.Size(447, 500);
+            this.Controls.Add(this.lblCapsLock);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmLogin_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblCapsLock;
+        private System.Windows.Forms.LinkLabel linkQMK;
+    }
+}
---
+++ 3_GUI/FrmLogin.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace _3_GUI
+{
+
+    public partial class FrmLogin : Form
+    {
+        string userName = "";
+        string passWord = "";
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        
+      
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            txtUsername.Text = "User Name";
+            txtUsername.ForeColor = Color.Gray;
+
+            txtPassWord.PasswordChar = '\0';
+            txtPassWord.Text = "Password";
+            txtPassWord.ForeColor = Color.Gray;
+
+            bool capsLock = Console.CapsLock;
+            if (capsLock == true)
+                lblCapsLock.Text = "Bạn đang bật Capslock";
+            else
+                lblCapsLock.Text = "";
+
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtUsername.ForeColor = Color.Black;
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            userName = txtUsername.Text;
+            if (userName.Equals("User Name"))
+            {
+                txtUsername.Text = "User Name";
+                txtUsername.ForeColor = Color.Gray;
+            }
+            else
+            {
+                if (userName.Equals(""))
+                {
+                    txtUsername.Text = "User Name";
+                    txtUsername.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    txtUsername.Text = userName;
+                    txtUsername.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void txtPassWord_Enter(object sender, EventArgs e)
+        {
+            txtPassWord.Text = "";
+            txtPassWord.ForeColor = Color.Black;
+            txtPassWord.PasswordChar = '*';
+
+        }
+
+        private void txtPassWord_Leave(object sender, EventArgs e)
+        {
+            passWord = txtPassWord.Text;
+            if (passWord.Equals("Password"))
+            {
+                txtPassWord.Text = "Password";
+                txtPassWord.ForeColor = Color.Gray;
+            }
+            else
+            {
+                if (passWord.Equals(""))
+                {
+                    txtPassWord.PasswordChar = '\0';
+                    txtPassWord.Text = "Password";
+                    txtPassWord.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    txtPassWord.PasswordChar = '*';
+                    txtPassWord.Text = passWord;
+                    txtPassWord.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            if (result == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                this.txtUsername.Focus();
+                this.txtUsername.Select(0, this.txtUsername.Text.Length);
+            }
+            if(e.KeyCode == Keys.CapsLock)
+            {
+                bool capsLock = Console.CapsLock;
+                if (capsLock == true)
+                    lblCapsLock.Text = "Bạn đang bật Capslock";
+                else
+                    lblCapsLock.Text = "";
+            }
+        }
+
+        private void txtPassWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                this.txtPassWord.Focus();
+                this.txtPassWord.Select(0, this.txtPassWord.Text.Length);
+            }
+            if (e.KeyCode == Keys.CapsLock)
+            {
+                bool capsLock = Console.CapsLock;
+                if (capsLock == true)
+                    lblCapsLock.Text = "Bạn đang bật Capslock";
+                else
+                    lblCapsLock.Text = "";
+            }
+        }
+
+        private void FrmLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.CapsLock)
+            {
+                bool capsLock = Console.CapsLock;
+                if (capsLock == true)
+                    lblCapsLock.Text = "Bạn đang bật Capslock";
+                else
+                    lblCapsLock.Text = "";
+            }
+        }
+    }
+
+}
