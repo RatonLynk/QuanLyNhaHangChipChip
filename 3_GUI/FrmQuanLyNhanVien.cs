87d10f5e45916891b6a0443d16d6ae24d16da06d
Vu:D <ratonlynkz@gmail.com>
2021-11-23 23:27:01 +0700

    ok

--- 3_GUI/FrmQuanLyNhanVien.cs
+++ 3_GUI/FrmQuanLyNhanVien.cs
@@ -19,6 +19,7 @@ namespace _3_GUI
         private IQLNhanVienService _iQlNhanVienService = new QLNhanVienService();
         private DatabaseContext _dbconContext;
         private Utilities _utilities;
+        private int _id;
         public FrmQuanLyNhanVien()
         {
             InitializeComponent();
@@ -27,6 +28,7 @@ namespace _3_GUI
             dgrid_NhanVien.Columns["MANV"].Visible = false;
             rbtnHDnhanvien.Checked = true;
             chk_nhanVien.Checked = true;
+            chk_nam.Checked = true;
 
         }
         void loadData()
@@ -52,99 +54,81 @@ namespace _3_GUI
         }
         private void dgrid_NhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            var index = e.RowIndex;
-            if (index == _iQlNhanVienService.getlstNhanViens().Count || index == -1)
-            {
-                txtMaNV.Text = null;
-                txt_TenNV.Text = null;
-                txtEmail.Text = null;
-                txtMatKhau.Text = null;
-                txt_SDT.Text = null;
-                txt_DiaChiNV.Text = null;
-                chk_quanLi.Checked = false;
-                chk_nhanVien.Checked = false;
-                chk_nam.Checked = false;
-                chk_nu.Checked = false;
-                rbtnHDnhanvien.Checked = false;
+            int rowindex = e.RowIndex;
+            _id = Convert.ToInt32(dgrid_NhanVien.Rows[rowindex].Cells[0].Value.ToString());
+            txt_TenNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[1].Value.ToString();
+            txtEmail.Text = dgrid_NhanVien.Rows[rowindex].Cells[2].Value.ToString();
+            txtMatKhau.Text = _utilities.GetHash(dgrid_NhanVien.Rows[rowindex].Cells[3].Value.ToString());
+            chk_nhanVien.Checked = dgrid_NhanVien.Rows[rowindex].Cells[4].Value.ToString() == "Nhân viên" ? true : false;
+            chk_quanLi.Checked = dgrid_NhanVien.Rows[rowindex].Cells[4].Value.ToString() == "Quản lí" ? true : false;
+            txt_SDT.Text = dgrid_NhanVien.Rows[rowindex].Cells[5].Value.ToString();
+            chk_nam.Checked = dgrid_NhanVien.Rows[rowindex].Cells[6].Value.ToString() == "Nam" ? true : false;
+            chk_nu.Checked = dgrid_NhanVien.Rows[rowindex].Cells[6].Value.ToString() == "Nữ" ? true : false;
+            txt_DiaChiNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[7].Value.ToString();
+            rbtnHDnhanvien.Checked = dgrid_NhanVien.Rows[rowindex].Cells[8].Value.ToString() == "Hoạt động" ? true : false;
+            rbtnKHDnhanvien.Checked = dgrid_NhanVien.Rows[rowindex].Cells[8].Value.ToString() == "Không Hoạt động" ? true : false;
+            txtMaNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[9].Value.ToString();
+            var nv = _iQlNhanVienService.getlstNhanViens().Where(c => c.MaNv == txtMaNV.Text).FirstOrDefault();
 
-                rbtnKHDnhanvien.Checked = false;
-                return;
+            if (nv.Status == true)
+            {
+                rbtnHDnhanvien.Checked = true;
             }
             else
             {
-                int rowindex = e.RowIndex;
-                txtMaNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[9].Value.ToString();
-                txt_TenNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[1].Value.ToString();
-                txtEmail.Text = dgrid_NhanVien.Rows[rowindex].Cells[2].Value.ToString();
-                txtMatKhau.Text = _utilities.GetHash(dgrid_NhanVien.Rows[rowindex].Cells[3].Value.ToString());
-                txt_SDT.Text = dgrid_NhanVien.Rows[rowindex].Cells[5].Value.ToString();
-                txt_DiaChiNV.Text = dgrid_NhanVien.Rows[rowindex].Cells[7].Value.ToString();
-                var nv = _iQlNhanVienService.getlstNhanViens().Where(c => c.MaNv == txtMaNV.Text).FirstOrDefault();
-                if (nv.Role == 0)
-                {
-                    chk_quanLi.Checked = true;
-                }
-                else
-                {
-                    chk_nhanVien.Checked = true;
-                }
-
-                if (nv.Sex == true)
-                {
-                    chk_nam.Checked = true;
-                }
-                else
-                {
-                    chk_nu.Checked = true;
-                }
-
-                if (nv.Status == true)
-                {
-                    rbtnHDnhanvien.Checked = true;
-                }
-                else
-                {
-                    rbtnKHDnhanvien.Checked = true;
-                }
+                rbtnKHDnhanvien.Checked = true;
             }
+            if (nv.Role == 1)
+            {
+                chk_nhanVien.Checked = true;
+            }
+            else
+            {
+                chk_quanLi.Checked = true;
+            }
+
+
         }
 
         private void btnThemNV_Click(object sender, EventArgs e)
         {
-           
+
             NhanVien NhanVien = new NhanVien();
             NhanVien.Id = dgrid_NhanVien.Rows.Cast<DataGridViewRow>()
                 .Max(r => Convert.ToInt32(r.Cells["Id"].Value)) + 1;
             NhanVien.MaNv = "NV" + NhanVien.Id;
-            NhanVien.Name =  txt_TenNV.Text;
+            NhanVien.Name = txt_TenNV.Text;
             NhanVien.Email = txtEmail.Text;
             NhanVien.Address = txt_DiaChiNV.Text;
             NhanVien.PhoneNo = txt_SDT.Text;
             NhanVien.Sex = Convert.ToBoolean(chk_nam.Checked ? true : false);
             NhanVien.Status = Convert.ToBoolean(rbtnHDnhanvien.Checked ? true : false);
             NhanVien.Password = _utilities.GetHash("123");
-            NhanVien.Role = chk_quanLi.Checked?0:1;
+            NhanVien.Role = chk_quanLi.Checked ? 0 : 1;
             if ((MessageBox.Show("Bạn muốn thêm một nhân viên ?",
                 "Thông báo",
                 MessageBoxButtons.YesNo) == DialogResult.Yes))
             {
                 _iQlNhanVienService.Add(NhanVien);
+                MessageBox.Show("Thêm thành công", "Thông báo");
                 loadData();
             }
         }
 
-        
+
 
         private void btnXoaNV_Click(object sender, EventArgs e)
         {
-            var nhanVien = _iQlNhanVienService.getlstNhanViens().Where(c => c.MaNv == txtMaNV.Text ).FirstOrDefault();
+            var nhanVien = _iQlNhanVienService.getlstNhanViens().FirstOrDefault(x => x.Id == _id);
             rbtnKHDnhanvien.Checked = true;
-            nhanVien.Role = 1;
+            //nhanVien.Role = 1;
             if ((MessageBox.Show("Bạn có chắc chắc sẽ dùng chức năng trên?",
                 "Thông báo !!!!!!!!!!!!!!!",
                 MessageBoxButtons.YesNo) == DialogResult.Yes))
             {
                 _iQlNhanVienService.Delete(nhanVien);
+                _iQlNhanVienService.Save();
+                MessageBox.Show("Xóa thành công", "Thông báo");
                 loadData();
             }
         }
@@ -155,7 +139,7 @@ namespace _3_GUI
             nhanVien.Name = txt_TenNV.Text;
             nhanVien.Email = txtEmail.Text;
             nhanVien.Password = _utilities.GetHash(txtMatKhau.Text);
-            nhanVien.Role = chk_quanLi.Checked ? 0:1;
+            nhanVien.Role = chk_quanLi.Checked ? 0 : 1;
             nhanVien.Address = txt_DiaChiNV.Text;
             nhanVien.PhoneNo = txt_SDT.Text;
             nhanVien.Sex = chk_nam.Checked ? true : false;
@@ -165,8 +149,10 @@ namespace _3_GUI
                 MessageBoxButtons.YesNo) == DialogResult.Yes))
             {
                 _iQlNhanVienService.Update(nhanVien);
-                loadData();
             }
+            MessageBox.Show("Sửa thành công", "Thông báo");
+            loadData();
+
         }
 
         private void chk_quanLi_CheckedChanged_1(object sender, EventArgs e)
@@ -190,6 +176,33 @@ namespace _3_GUI
         private void btn_Luu_Click(object sender, EventArgs e)
         {
             _iQlNhanVienService.Save();
+        }
+
+        private void txt_TenNV_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txt_TenNV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên của bạn.");
+                return;
+            }
+        }
+
+        private void txtEmail_Validating(object sender, CancelEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Vui lòng nhập email của bạn.");
+                return;
+            }
+        }
+        //clear form các thứ
+        private void Btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_TenNV.ResetText();
+            txtEmail.ResetText();
+            txt_SDT.ResetText();
+            txt_DiaChiNV.ResetText();
+            txtMatKhau.ResetText();
         }
     }
 }
