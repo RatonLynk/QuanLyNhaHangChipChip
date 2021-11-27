bc4f00e7296ba97f6d2f27df22aee44864d535ec
chidpph <phuongchi140295@gmail.com>
2021-11-27 12:05:29 +0700

    ád

--- 3_GUI/FrmQuanLyHoaDon.cs
+++ 3_GUI/FrmQuanLyHoaDon.cs
@@ -73,6 +73,7 @@ namespace _3_GUI
             if ((rowIndex == _qlHoaDon.GetBillsFromDB().Count) || rowIndex == -1) return;
             _idHoaDon = Convert.ToInt32(Dgid_HoaDon.Rows[rowIndex].Cells[5].Value.ToString());
             LoadHoaDonChiTiet(_idHoaDon);
+            //ádsdsa
         }
     }
 }
